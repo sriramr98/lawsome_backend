48b25ef69ac5f6ff1e9b0454b58d9b335b1c9977
R.Sriram <r.sriram@games24x7.com>
2023-06-30 14:34:43 +0530

    added autocreate db when starting up server.

--- server/index.ts
+++ server/index.ts
@@ -7,6 +7,7 @@ dotenv.config();
 import initRoutes from "./routes/init";
 import initSocket from "./sockets/init";
 import initFirebase from "./config/firebase";
+import getSequelize from "./entities/init";
 
 const app = express();
 const server = http.createServer(app);
@@ -19,5 +20,6 @@ initSocket(io);
 app.use(initRoutes);
 
 server.listen(3000, () => {
+  getSequelize().sync();
   console.log("listening on *:3000");
 });
