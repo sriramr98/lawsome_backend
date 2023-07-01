f846ec1a6151741863a11fec55d1c23a984a65ec
Divyanshu Bhatnagar <divyanshu.bhatnagar@adpushup.com>
2023-07-01 12:50:43 +0530

    Changes to integrate UI

--- server/index.ts
+++ server/index.ts
@@ -6,20 +6,26 @@ import dotenv from "dotenv";
 dotenv.config();
 import initRoutes from "./routes/init";
 import initSocket from "./sockets/init";
-import initFirebase from "./config/firebase";
+// import initFirebase from "./config/firebase";
 import getSequelize from "./entities/init";
 
 const app = express();
 const server = http.createServer(app);
 
-initFirebase();
+// initFirebase();
 
-const io = new Server(server);
+// required for CORS
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
 initSocket(io);
 
 app.use(initRoutes);
 
-server.listen(3000, () => {
-  getSequelize().sync();
-  console.log("listening on *:3000");
+server.listen(8000, () => {
+    getSequelize().sync();
+    console.log("listening on *:8000");
 });
