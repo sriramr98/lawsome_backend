551b277f709f89e13be6cd6bf70387cb7f1667c4
R.Sriram <r.sriram@games24x7.com>
2023-07-26 17:25:05 +0530

    added gpt4 support

--- src/config/config.ts
+++ src/config/config.ts
@@ -20,6 +20,7 @@ export default () => ({
         azureApiDeploymentName: process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME,
         azureApiVersion: process.env.AZURE_OPENAI_API_VERSION,
         azureApiInstanceName: process.env.AZURE_OPENAI_API_INSTANCE_NAME,
+        openaiApiKey: process.env.OPENAI_API_KEY,
     },
     sentry: {
         dsn: process.env.SENTRY_DSN,
